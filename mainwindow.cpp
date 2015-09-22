6d733e8797325ce5ce74b163581744e565fdb5d3
koenvanvliet <koen@koenvanvliet.com>
2015-09-22 17:47:35 +0200

    Update

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,6 +1,5 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
-#include <string.h>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -14,4 +13,26 @@ MainWindow::MainWindow(QWidget *parent) :
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::on_pushButton_clicked()
+{
+    qApp->quit();
+}
+
+void MainWindow::on_actionOpen_triggered()
+{
+    QString fileName = QFileDialog::getOpenFileName(this, tr("Open File"), QString(),
+               tr("Text Files (*.txt);;C++ Files (*.cpp *.h)"));
+
+       if (!fileName.isEmpty()) {
+           QFile file(fileName);
+           if (!file.open(QIODevice::ReadOnly)) {
+               QMessageBox::critical(this, tr("Error"), tr("Could not open file"));
+               return;
+           }
+           QTextStream in(&file);
+           ui->textBrowser->setText(in.readAll());
+           file.close();
+       }
 }
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,11 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QFile>
+#include <QFileDialog>
+#include <QMessageBox>
+#include <QTextStream>
+#include <QString>
 
 namespace Ui {
 class MainWindow;
@@ -14,6 +19,11 @@ class MainWindow : public QMainWindow
 public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
+
+private slots:
+    void on_pushButton_clicked();
+
+    void on_actionOpen_triggered();
 
 private:
     Ui::MainWindow *ui;
